b0d3c26f6a1f4c1eb7994d3db66dd8f1da4dec49
Hukyc <nukutac@gmail.com>
2023-04-21 13:08:50 +0300

    Service added

--- app/src/main/java/com/example/mobdevpract5/MyService.java
+++ app/src/main/java/com/example/mobdevpract5/MyService.java
@@ -10,7 +10,11 @@ public class MyService extends Service {
 
     @Override
     public IBinder onBind(Intent intent) {
-        // TODO: Return the communication channel to the service.
         throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
     }
 }
