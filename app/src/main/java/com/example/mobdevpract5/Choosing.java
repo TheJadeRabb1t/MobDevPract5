1b1c997d3fcbe66dc3bbab9b4ebf1727eb493fa3
Hukyc <nukutac@gmail.com>
2023-04-21 12:47:10 +0300

    Notification display added

--- app/src/main/java/com/example/mobdevpract5/Choosing.java
+++ app/src/main/java/com/example/mobdevpract5/Choosing.java
@@ -1,7 +1,10 @@
 package com.example.mobdevpract5;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -10,10 +13,22 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class Choosing extends Fragment {
+    private static final int NOTIFY_ID = 0;
+    private final String CHANNEL_ID = "channel_id";
     View view;
 
+    @SuppressLint("MissingPermission")
+    public void showNotification(){
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Реализация уведомления")
+                .setContentText("Продолжение реализации уведомления")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(getContext());
+        notificationManagerCompat.notify(NOTIFY_ID, builder.build());
+    }
+
     public Choosing() {
-        // Required empty public constructor
     }
 
     @Override
@@ -29,6 +44,9 @@ public class Choosing extends Fragment {
             Bundle bundle = new Bundle();
             bundle.putString("buttonPressing", "1");
             Navigation.findNavController(view).navigate(R.id.action_choosing_to_registration, bundle);
+        });
+        view.findViewById(R.id.notificationButton).setOnClickListener(v ->{
+            showNotification();
         });
         return view;
     }
